2f514d64b78ecb7a0dddf9c463bbfab752f90416
xysong <xysong25@gmail.com>
2022-10-24 19:45:55 -0400

    contour finder done

---
+++ image_processing/contour.py
@@ -0,0 +1,8 @@
+import cv2 as cv
+import torch
+import numpy as np
+from icecream import ic
+
+
+if __name__ == '__main__':
+    ic("contour.py")
---
+++ models/img_autoencoder.py
@@ -0,0 +1,25 @@
+import torch
+from torchinfo import summary
+import torchvision
+import torch.nn as nn
+from icecream import ic
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def __forward__(self):
+        pass
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        pass
+
+    def __forward__(self):
+        pass
+
+
+if __name__ == '__main__':
+    ic("CT Image Autoencoder Backbone")
