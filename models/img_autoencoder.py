f812f7364c85ab9b09efc15805bf2b54f762f3d6
xysong <xysong25@gmail.com>
2022-10-25 23:01:26 -0400

    autoencoder architecture done

--- models/img_autoencoder.py
+++ models/img_autoencoder.py
@@ -5,49 +5,64 @@ import torch.nn as nn
 import torch.nn.functional as F
 from icecream import ic
 
+DEVICE = 'cuda' if torch
 
 class Encoder(nn.Module):
-    def __init__(self, hidden_dim):
-        # Credit: This architecture is obtained from Xiaoyang's previous research project
-        super(Encoder, self).__init__()
-        self.d = hidden_dim
-        self.conv1 = nn.Conv2d(1, 32, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        # Fully connected layer
-        self.fc1 = nn.Linear(64 * 4 * 4, 512)
-        self.fc2 = nn.Linear(512, hidden_dim)  # 512 -> 128
+    def __init__(self):
+        super().__init__()
+        self.conv2d1 = nn.Conv2d(1, 16, kernel_size=(7, 7), padding=(3, 3))
+        self.maxpool2d1 = nn.MaxPool2d((3, 3), return_indices=True)
+        self.conv2d2 = nn.Conv2d(16, 8, kernel_size=(7, 7), padding=(3, 3))
+        self.maxpool2d2 = nn.MaxPool2d((3, 3), return_indices=True)
+        self.flatten = nn.Flatten()
+        self.linear1 = nn.Linear(in_features=17496, out_features=2560)
+        self.linear2 = nn.Linear(in_features=2560, out_features=128)
+        self.linear3 = nn.Linear(in_features=128, out_features=64)
+        self.linear4 = nn.Linear(in_features=64, out_features=6)
 
     def forward(self, x):
-        x = self.pool(self.conv1(x))
-        x = self.pool(self.conv2(x))
-        x = x.view(-1, 64 * 4 * 4)  # flatten the tensor
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        batch_size = x.shape[0]
+        output = F.relu(self.conv2d1(x))
+        output, indices1 = self.maxpool2d1(output)
+        output = F.relu(self.conv2d2(output))
+        output, indices2 = self.maxpool2d2(output)
+        output = self.flatten(output)
+        output = F.relu(self.linear1(output))
+        output = F.relu(self.linear2(output))
+        output = F.relu(self.linear3(output))
+        output = self.linear4(output)
+        return output, indices1, indices2
 
 
 class Decoder(nn.Module):
-    def __init__(self, hidden_dim):
-        self.d = hidden_dim
-        self.model = nn.Sequential(
-            nn.Linear(self.d, 1024),
-            nn.ReLU(),
-            nn.BatchNorm1d(1024),
-            nn.Linear(1024, 7 * 7 * 128),
-            nn.ReLU(),
-            nn.BatchNorm1d(7 * 7 * 128),
-            nn.Unflatten(-1, (128, 7, 7)),
-            nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1),
-            nn.ReLU(),
-            nn.BatchNorm2d(64),
-            nn.ConvTranspose2d(64, 1, 4, stride=2, padding=1),
-            nn.Tanh()
-        )
+    def __init__(self):
+        super().__init__()
+        self.linear1 = nn.Linear(6, 64)
+        self.linear2 = nn.Linear(64, 128)
+        self.linear3 = nn.Linear(128, 2560)
+        self.linear4 = nn.Linear(2560, 17496)
+        self.maxunpool2d1 = nn.MaxUnpool2d((3, 3))
+        self.conv2d1 = nn.ConvTranspose2d(
+            8, 16, kernel_size=(7, 7), padding=(3, 3))
+        self.maxunpool2d2 = nn.MaxUnpool2d((3, 3))
+        self.conv2d2 = nn.ConvTranspose2d(
+            16, 1, kernel_size=(7, 7), padding=(3, 3))
+        self.sigmoid1 = nn.Sigmoid()
 
-    def forward(self, x):
-        return self.model(x)
+    def forward(self, x, indices1, indices2):
+        output = F.relu(self.linear1(x))
+        output = F.relu(self.linear2(output))
+        output = F.relu(self.linear3(output))
+        output = F.relu(self.linear4(output))
+        output = output.view((output.shape[0], 8, 27, 81))
+        output = self.maxunpool2d1(
+            output, indices2, output_size=torch.Size([output.shape[0], 8, 83, 243]))
+        output = F.relu(self.conv2d1(output))
+        output = self.maxunpool2d2(output, indices1, output_size=torch.Size([
+                                   output.shape[0], 16, 250, 730]))
+        output = self.conv2d2(output)
+        output = self.sigmoid1(output)
+        return output
 
 
 class AutoEncoder(nn.Module):
@@ -57,7 +72,9 @@ class AutoEncoder(nn.Module):
         self.decoder = Decoder(self.d)
 
     def forward(self, x):
-        return self.decoder(self.encoder(x))
+        output, indc1, indc2 = self.encoder(x)
+        output = self.decoder(output, indc1, indc2)
+        return output
 
 
 def train_autoencoder(model, num_epochs, optimizer, t_loader, device=DEVICE):
