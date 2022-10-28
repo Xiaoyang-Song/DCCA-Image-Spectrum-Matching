3a14aeb4f33842f83155751c6e6b2a39202be9b5
xysong <xysong25@gmail.com>
2022-10-28 11:21:00 -0400

    docstrings for processed_to_final

--- image_processing/contour.py
+++ image_processing/contour.py
@@ -1,3 +1,4 @@
+from multiprocessing import ProcessError
 import cv2 as cv
 import torch
 import numpy as np
@@ -7,26 +8,27 @@ import os
 from tqdm.notebook import tqdm
 
 
-
-def processed_to_final(image_addr_list, path,min_area, max_area):
+def processed_to_final(processed_path, save_path, min_area, max_area):
     """
     Convert processed denoised images to final defects-only images
     Args:
-        image_addr_list (str): 
-        path (_type_): _description_
-        min_area (_type_): _description_
-        max_area (_type_): _description_
+        processed_path (str): path to processed data (locally "../Data/processed/")
+        save_path (str): path for saving final data (locally "../Data/final/")
+        min_area (float): minimum area for poles
+        max_area (float): maximum area for poles
     """
     num_defects = []
+    image_addr_list = os.listdir(processed_path)
     # min_area, max_area = 20, 20000
     for i in tqdm(image_addr_list):
-        full_path = os.path.join(path, i)
+        full_path = os.path.join(processed_path, i)
         imgobj = Image.open(full_path).convert('RGB')
         img = np.asarray(imgobj)
         # Convert to grayscale images
-        grayimg = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+        grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
         # Find contours
-        contours, hierarchy = cv.findContours(grayimg,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv.findContours(
+            grayimg, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
         num_poles = 0
         valid_contours = []
@@ -43,9 +45,11 @@ def processed_to_final(image_addr_list, path,min_area, max_area):
                         valid_contours.append(contours[j])
         num_defects.append(num_poles)
         # Save to processed
-        mask = np.ones(img.shape[:2],dtype=np.uint8) * 255
-        img_arr = cv.drawContours(mask, valid_contours, -1, (0,255,255), -1)
-        Image.fromarray(img_arr).save('../Data/final/' + i)
+        mask = np.ones(img.shape[:2], dtype=np.uint8) * 255
+        img_arr = cv.drawContours(mask, valid_contours, -1, (0, 255, 255), -1)
+        Image.fromarray(img_arr).save(save_path + i)
+
+
 # TODO: wrap up code in image_processing.ipynb into this file
 if __name__ == '__main__':
     ic("contour.py")
