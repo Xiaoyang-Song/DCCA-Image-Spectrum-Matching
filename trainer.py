798241cfd7ae7e72d6222688b733d4c5114d8b32
xysong <xysong25@gmail.com>
2022-11-23 04:34:57 -0500

    trainer done

--- trainer.py
+++ trainer.py
@@ -11,13 +11,45 @@ from icecream import ic
 def train(model, tri_dset, val_dset, optimizer,
           max_epoch, writer):
     ic("Training begins")
+    criterion = CCA()
+
+    model.train()
+
+    tri_iter_count, val_iter_count = 0, 0
     for epoch in max_epoch:
-        for (img, spec) in tri_dset:
+        for step, (img, spec) in enumerate(tri_dset):
             img = img.unsqueeze(0).to(DEVICE)
             spec = spec.unsqueeze(0).to(DEVICE)
-            # Feed in LSTM
 
+            # Zero Existing Gradients
+            optimizer.zero_grad()
+            # WindowDCCA Forward Pass
             out1, out2 = model(img, spec)
+            loss = criterion(out1, out2)
+            # Backward Pass
+            loss.backward()
+            # Gradient Update
+            optimizer.step()
+
+            # Log statistics
+            writer.add_scalar("Loss/Train", loss.detach(), tri_iter_count)
+            tri_iter_count += 1
+            print(
+                f"Epoch {epoch:<3} | Step {step:<4} | Train Loss: {loss:.5f}")
+
+        # Evaluation
+        with torch.no_grad:
+            for step, (img, spec) in enumerate(val_dset):
+                img = img.unsqueeze(0).to(DEVICE)
+                spec = spec.unsqueeze(0).to(DEVICE)
+                out1, out2 = model(img, spec)
+                loss = criterion(out1, out2)
+                # Log validation statistics
+                writer.add_scalar("Loss/Eval", loss.detach(), val_iter_count)
+                val_iter_count += 1
+                print(
+                    f"Epoch {epoch:<3} | Step {step:<4} | Eval Loss: {loss:.5f}")
+
 
 if __name__ == '__main__':
     ic("Training...")
