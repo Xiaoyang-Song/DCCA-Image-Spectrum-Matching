fda2aa34e65213cf9c2b073571a8d97db06c747a
xysong <xysong25@gmail.com>
2022-11-23 00:48:37 -0500

    img lstm setup

--- models/img_window_lstm.py
+++ models/img_window_lstm.py
@@ -10,12 +10,34 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 class ImgLSTM(nn.Module):
-    def __init__(self):
+    def __init__(self, input_dim, hidden_dim, num_layers):
         super().__init__()
+        # Config
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        # Model Declaration
+        self.lstm = nn.LSTM(input_size=self.input_dim,
+                            hidden_size=self.hidden_dim,
+                            num_layers=self.num_layers,
+                            batch_first=True)
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        # For image LSTM, input features should be of shape (B, L, hidden_dim)
+        assert len(x.shape) == 3
+        assert x.shape[-1] == self.input_dim
+        # Forward pass
+        output, (hn, cn) = self.lstm(x)
+        # hn, cn: (B, self.num_layers, self.hidden_dim)
+        return output, hn, cn
 
 
 if __name__ == '__main__':
     ic("Processing window of images")
+    # Simple sanity check
+    lstm = ImgLSTM(6, 64, 1)
+    x = torch.zeros((3, 5, 6))
+    output, hn, cn = lstm(x)
+    ic(output.shape)
+    ic(hn.shape)
+    ic(cn.shape)
